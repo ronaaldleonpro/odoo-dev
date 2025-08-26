949e55c5c0a21f2f2033bd1ff32cdb4780bc7297
Ronald Leon <ronaldleonpro@gmail.com>
2025-08-26 14:58:48 -0600

    feat: correct use for inherit form for upload PDFs in quote builder

--- addons/scope_management/__manifest__.py
+++ addons/scope_management/__manifest__.py
@@ -6,11 +6,12 @@
     'version': '1.0',
     'category': 'Productivity/Scopes',
     'summary': 'Organize your quotazion with scopes',
-    'depends': ['base', 'sale', 'stock', 'crm', 'product', 'web', 'mail', 'sale_management','sale_pdf_quote_builder'],
+    'depends': ['base', 'sale', 'stock', 'crm', 'product', 'web', 'mail', 'sale_management','sale_pdf_quote_builder',],
     'auto_install': True,
     'data': [
         'security/ir.model.access.csv',
         'views/sales_team_views.xml',
+        'views/sales_team_document_views.xml',
         'views/sales_order_views.xml',
         'views/product_template_views.xml',
         'views/scope_management_views.xml',
