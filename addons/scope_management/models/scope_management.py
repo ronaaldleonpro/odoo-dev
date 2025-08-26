8426f6da643b174c71fd40606bc4339b7c3b4b2c
ronaldleonpro <ronaldleonpro@gmail.com>
2025-08-25 22:12:28 -0600

    Limpiando código inservible de scope_management module and view

--- addons/scope_management/models/scope_management.py
+++ addons/scope_management/models/scope_management.py
@@ -2,20 +2,16 @@ from odoo import api, models, fields
 from odoo.tools import html2plaintext
 
 class ScopeManagement(models.Model):
+    #Model name & description. Inherits from models.Model (Odoo base class)
     _name = 'scope.management'
     _description = 'Scopes Management'
 
     name = fields.Char(string='Service', required=True)
     description = fields.Html(string='Description')
-    product_id = fields.Many2one('product.product', string='Product')
-    product_ids = fields.One2many('product.template', 'scope_id', string='Products')
     sales_team = fields.Many2one('crm.team', string='Sales Team')
-    state = fields.Selection([
-        ('draft', 'Draft'),
-        ('in_progress', 'In Progress'),
-        ('completed', 'Completed'),
-        ('canceled', 'Canceled'),
-    ], string="State", default='draft')
+    product_id = fields.Many2one('product.product', string='Product')
+
+    product_ids = fields.One2many('product.template', 'scope_id', string='Products')
     active = fields.Boolean(default=True)
 
     sale_order_id = fields.Many2one("sale.order", string="Sale Order", ondelete="cascade")
@@ -23,26 +19,13 @@ class ScopeManagement(models.Model):
     @api.model_create_multi
     def create(self, vals_list):
         for vals in vals_list:
-            if not vals.get('name') and vals.get('description'):
+            if not vals.get('description'):
                 text = html2plaintext(vals['description'])
                 name = text.strip().replace('*', '').partition("\n")[0]
                 vals['name'] = (name[:97] + '...') if len(name) > 100 else name
         return super().create(vals_list)
 
-    def action_mark_completed(self):
-        self.write({'state': 'completed'})
-
-    def action_mark_in_progress(self):
-        self.write({'state': 'in_progress'})
-
-    def action_mark_canceled(self):
-        self.write({'state': 'canceled'})
-
-    def action_reopen(self):
-        self.write({'state': 'draft'})
-
     
 class ProductProduct(models.Model):
     _inherit = 'product.product'
-
     scope_ids = fields.One2many('scope.management', 'product_id', string='Scopes')
